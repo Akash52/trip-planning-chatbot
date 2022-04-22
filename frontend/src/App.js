1d612dd886b26a99ea34933a74e4fccd191d62bc
Akash52 <ac8572611@gmail.com>
2022-04-22 12:31:33 +0530

    routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,16 @@
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import About from './components/About';
+
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomeScreen />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
